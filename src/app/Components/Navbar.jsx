52348b3d7c3dedf9a43e1ee9633633127a67a21f
Rohan Hiray <rohan.hiray23@mmit.edu,in>
2026-02-25 23:14:16 +0530

    navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,23 +1,54 @@
-import React from 'react'
-import Image from 'next/image'
+'use client'
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+
 
 const Navbar = () => {
+
+   const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const today = new Date();
+    setDate(today.toLocaleDateString());
+  }, []);
+
+
+
   return (
     <>
-    <div className='Navbar'>
-    <button>
-      <Image
-  src={"Menu.svg"}
-  alt="Example"
-  width={30}
-  height={30}
-/>
-    </button>
-<h1>Hi</h1>
-    </div>
+      <div className="Navbar flex justify-between items-center w-full h-16">
+        <div className="MenuBar-SearchBar ">
+          <ul className="flex  gap-6 list-none p-2">
+            <li>
+          <button>
+            <Image src={"Menu.svg"} alt="Example" width={10} height={10} />
+          </button>
+          </li>
+          <li>
+          <form action="">
+            <input
+              title="Search"
+              type="text"
+              placeholder="Search"
+              name="Search"
+              id="Search"
+            />
+          </form>
+          </li>
+          </ul>
+        </div>
+        <div className="Profile-Date-mail">
+          <ul className="flex  gap-6 list-none p-2">
+            <li><button>{date}</button></li>
+           <li> <button>email</button></li>
+           <li><button>notification</button></li>
+           <li><button>profile</button></li>
+          
+          </ul>
+        </div>
+      </div>
     </>
-   
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
